b1c1758ba4373398e60095e21ba1df5717605dcd
giorgiofrittoli <giorgio.frittoli@gmail.com>
2018-10-06 16:48:28 +0200

    implemented toString method

--- src/main/java/fritz/test/recipe/model/Difficulty.java
+++ src/main/java/fritz/test/recipe/model/Difficulty.java
@@ -1,5 +1,26 @@
 package fritz.test.recipe.model;
 
 public enum Difficulty {
-	EASY,MEDIUM,HARD,MODERATE
+	EASY {
+		@Override
+		public String toString() {
+			return "Easy";
+		}
+	},
+	MEDIUM {
+		@Override
+		public String toString() {
+			return "Medium";
+		}
+	}, HARD {
+		@Override
+		public String toString() {
+			return "Hard";
+		}
+	}, MODERATE {
+		@Override
+		public String toString() {
+			return "Moderate";
+		}
+	}
 }
